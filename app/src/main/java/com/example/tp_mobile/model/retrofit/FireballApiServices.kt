d1317b7ed0e3b7d54e1d23590ad48b3ddb297c31
BlackArrowEtu <nathan.miconi@etu.univ-lyon1.fr>
2024-09-13 13:52:23 +0200

    Stets

--- app/src/main/java/com/example/tp_mobile/model/retrofit/FireballApiServices.kt
+++ app/src/main/java/com/example/tp_mobile/model/retrofit/FireballApiServices.kt
@@ -3,9 +3,10 @@ package com.example.tp_mobile.model.retrofit
 import com.example.example.FireballApiResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
-import retrofit2.Call
 
 interface FireballApiService{
     @GET("nasa-fireball-and-bolide-reports@datastro/records")
     suspend fun getFireballData(@Query("limit") limit: Int): FireballApiResponse
+
+
 }
