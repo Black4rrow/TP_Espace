46f3f370dbaaba67be4be0a67d7582465abe4633
BlackArrowEtu <nathan.miconi@etu.univ-lyon1.fr>
2024-09-13 17:40:16 +0200

    Fragment mis en place !

--- app/src/main/java/com/example/tp_mobile/views/FireballNavigationControllerActivity.kt
+++ app/src/main/java/com/example/tp_mobile/views/FireballNavigationControllerActivity.kt
@@ -1,4 +1,4 @@
-package com.example.tp_mobile.views
+package com.example.tp_mobile.views.fireball
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
@@ -8,9 +8,9 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.ViewModelProvider
 import com.example.tp_mobile.R
 import com.example.tp_mobile.model.retrofit.FireballApiController
-import com.example.tp_mobile.viewmodel.Fireball.FireballListViewModel
+import com.example.tp_mobile.viewmodel.FireballListViewModel
 
-class FireballListViewActivity : AppCompatActivity() {
+class FireballNavigationController : AppCompatActivity() {
 
     val fireballApiController = FireballApiController()
     private lateinit var viewModel: FireballListViewModel
@@ -19,7 +19,8 @@ class FireballListViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_fireball_list_view_activuty)
+
+        setContentView(R.layout.fragment_fireball_navigation_controller)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
---
+++ app/src/main/java/com/example/tp_mobile/views/fireball/FireballListFragment.kt
@@ -0,0 +1,54 @@
+package com.example.tp_mobile.views.fireball
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.tp_mobile.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [FireballListFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class FireballListFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fireball_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() =
+            FireballListFragment().apply {
+                arguments = Bundle().apply {
+                }
+            }
+    }
+}
