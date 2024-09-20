47d9c564d180ef5ab69b3e8653a870959ef84e1e
yannis royer <royeryannis@gmail.com>
2024-09-20 17:31:49 +0200

    login new account non fini

--- app/src/main/java/com/example/tp_mobile/SplashScreen.kt
+++ app/src/main/java/com/example/tp_mobile/SplashScreen.kt
@@ -2,12 +2,12 @@ package com.example.tp_mobile
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.os.Handler
+import android.os.Looper
 
 class SplashScreen : AppCompatActivity() {
 
