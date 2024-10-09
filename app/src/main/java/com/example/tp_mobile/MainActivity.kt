d8935451bcac8f9cdc492e226497c2a7e3457cbe
BlackArrowEtu <nathan.miconi@etu.univ-lyon1.fr>
2024-10-09 15:00:14 +0200

    mise en place navBar

--- app/src/main/java/com/example/tp_mobile/MainActivity.kt
+++ app/src/main/java/com/example/tp_mobile/MainActivity.kt
@@ -7,11 +7,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.tp_mobile.databinding.ActivityMainBinding
+import com.example.tp_mobile.views.APODNavigationControllerActivity
 import com.example.tp_mobile.views.FireballNavigationControllerActivity
+import com.example.tp_mobile.views.login.LoginActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    lateinit var bottomNavigationView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +35,39 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        setUpNavBar()
+
+    }
+
+    private fun setUpNavBar() {
+        bottomNavigationView = findViewById(R.id.navBar)
+        bottomNavigationView.setOnItemSelectedListener {
+            try {
+                when (it.itemId) {
+                    R.id.home -> {
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                        true
+                    }
+
+                    R.id.apod -> {
+                        val intent = Intent(this, APODNavigationControllerActivity::class.java)
+                        startActivity(intent)
+                        true
+                    }
+
+                    R.id.profile -> {
+                        val intent = Intent(this, LoginActivity::class.java)
+                        startActivity(intent)
+                        true
+                    }
+
+                    else -> false
+                }
+            } catch (e: Exception) {
+                throw e
+            }
+        }
     }
 
 }
--- app/src/main/java/com/example/tp_mobile/views/APODNavigationControllerActivity.kt
+++ app/src/main/java/com/example/tp_mobile/views/APODNavigationControllerActivity.kt
@@ -2,54 +2,36 @@ package com.example.tp_mobile.views
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.lifecycle.ViewModelProvider
 import com.example.tp_mobile.MainActivity
 import com.example.tp_mobile.R
-import com.example.tp_mobile.model.domain.api.FireballApiController
-import com.example.tp_mobile.views.fireball.FireballListViewModel
-import com.example.tp_mobile.views.fireball.FireballSectionFragment
 import com.example.tp_mobile.views.login.LoginActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class FireballNavigationControllerActivity : AppCompatActivity() {
+class APODNavigationControllerActivity : AppCompatActivity() {
 
-    val fireballApiController = FireballApiController
-    private lateinit var viewModel: FireballListViewModel
-    val fragmentManager = supportFragmentManager
-    val fragmentTransaction = fragmentManager.beginTransaction()
     lateinit var bottomNavigationView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
-        setContentView(R.layout.activity_fireball_navigation_controller)
+        setContentView(R.layout.activity_apodnavigation_controller)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        viewModel = ViewModelProvider(this)[FireballListViewModel::class.java]
-
-
-        fragmentTransaction.replace(
-            R.id.fireballFragmentContainer,
-            FireballSectionFragment.newInstance()
-        )
-        fragmentTransaction.commit()
-
         setUpNavBar()
     }
 
     private fun setUpNavBar() {
         bottomNavigationView = findViewById(R.id.navBar)
-        bottomNavigationView.selectedItemId = View.NO_ID
+        bottomNavigationView.selectedItemId = R.id.apod
+
         bottomNavigationView.setOnItemSelectedListener {
             try {
                 when (it.itemId) {
@@ -78,6 +60,4 @@ class FireballNavigationControllerActivity : AppCompatActivity() {
             }
         }
     }
-
-
 }
