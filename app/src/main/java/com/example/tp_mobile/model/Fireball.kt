41437f9e3ef36e713ec7f058e7bce4b6282dffa0
yannis royer <royeryannis@gmail.com>
2024-11-06 16:59:49 +0100

    cratere +appel api avec _

--- app/src/main/java/com/example/tp_mobile/model/Fireball.kt
+++ app/src/main/java/com/example/tp_mobile/model/Fireball.kt
@@ -1,26 +1,27 @@
 package com.example.tp_mobile.model
 
 import com.example.tp_mobile.views.fireball.CustomFireballAdapter
+import com.google.gson.annotations.SerializedName
 import kotlinx.serialization.SerialName
 import java.io.Serializable
 
 data class FireballApiResponse(
-    @SerialName("total_count") var totalCount: Int? = null,
-    @SerialName("results") var results: ArrayList<Fireball> = arrayListOf()
+    @SerializedName("total_count") var totalCount: Int? = null,
+    @SerializedName("results") var results: ArrayList<Fireball> = arrayListOf()
 ) : Serializable
 
 
 data class Fireball(
-    @SerialName("date") var date: String? = null,
-    @SerialName("energy") var energy: Double? = null,
-    @SerialName("impact_e") var impactE: Double? = null,
-    @SerialName("vel") var vel: Int? = null,
-    @SerialName("alt") var alt: Double? = null,
-    @SerialName("lon") var lon: Int = 0,
-    @SerialName("lat") var lat: Int = 0,
-    @SerialName("lon_dir") var lonDir: String? = null,
-    @SerialName("lat_dir") var latDir: String? = null,
-    @SerialName("coord") var coord: Coord? = Coord(),
+    @SerializedName("date") var date: String? = null,
+    @SerializedName("energy") var energy: Double? = null,
+    @SerializedName("impact_e") var impactE: Double? = null,
+    @SerializedName("vel") var vel: Int? = null,
+    @SerializedName("alt") var alt: Double? = null,
+    @SerializedName("lon") var lon: Int = 0,
+    @SerializedName("lat") var lat: Int = 0,
+    @SerializedName("lon_dir") var lonDir: String? = null,
+    @SerializedName("lat_dir") var latDir: String? = null,
+    @SerializedName("coord") var coord: Coord? = Coord(),
 ) : Serializable {
     var isFavorite: Boolean = false
 
@@ -30,8 +31,8 @@ data class Fireball(
 }
 
 data class Coord(
-    @SerialName("lon") var lon: Double? = null,
-    @SerialName("lat") var lat: Double? = null
+    @SerializedName("lon") var lon: Double? = null,
+    @SerializedName("lat") var lat: Double? = null
 ) : Serializable
 
 interface OnFireballFavoriteListener {
--- app/src/main/java/com/example/tp_mobile/views/fireball/FireballViewFragment.kt
+++ app/src/main/java/com/example/tp_mobile/views/fireball/FireballViewFragment.kt
@@ -1,18 +1,21 @@
 package com.example.tp_mobile.views.fireball
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.tp_mobile.model.Fireball
 import com.example.tp_mobile.R
 import com.example.tp_mobile.databinding.FragmentFireballViewBinding
+import com.example.tp_mobile.model.Fireball
+import java.math.BigDecimal
+import java.math.RoundingMode
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
+import kotlin.math.pow
+import kotlin.math.roundToInt
 
 class FireballViewFragment : Fragment() {
 
@@ -30,7 +33,8 @@ class FireballViewFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_fireball_view, container, false)
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_fireball_view, container, false)
         return binding.root
     }
 
@@ -50,11 +54,11 @@ class FireballViewFragment : Fragment() {
             mapButtonClicked()
         }
 
-        binding.favIcon.setOnClickListener{
+        binding.favIcon.setOnClickListener {
             favButtonClicked()
         }
 
-        viewModel.isFavorite(fireball!!).observe( viewLifecycleOwner){
+        viewModel.isFavorite(fireball!!).observe(viewLifecycleOwner) {
             if (it) {
                 binding.favIcon.setImageResource(R.drawable.baseline_favorite_border_24)
                 binding.favIcon.setColorFilter(R.color.black)
@@ -70,13 +74,30 @@ class FireballViewFragment : Fragment() {
         binding.radiatedValue.text = "${fireball?.energy ?: "?"}e10 J"
         binding.impactValue.text = "${fireball?.impactE ?: "?"}kt"
         binding.altitudeValue.text = "${fireball?.alt ?: "?"} m"
-        binding.radiusValue.text = "${calculateCraterRadius(fireball?.impactE ?: 0.0, fireball?.alt ?: 0.0, fireball?.energy ?: 0.0, fireball?.vel?.toDouble() ?: 0.0)} m"
-        binding.coordinatesValue.text = "${fireball?.lon ?: "?"} ${fireball?.lonDir ?: "?"} \n${fireball?.lat ?: "?"} ${fireball?.latDir ?: "?"}"
+        binding.radiusValue.text = "${
+            calculateCraterRadius(
+                fireball?.impactE ?: 0.0,
+                fireball?.alt ?: 0.0,
+                fireball?.energy ?: 0.0,
+                fireball?.vel?.toDouble() ?: 0.0
+            )
+        } m"
+        binding.coordinatesValue.text =
+            "${fireball?.lon ?: "?"} ${fireball?.lonDir ?: "?"} \n${fireball?.lat ?: "?"} ${fireball?.latDir ?: "?"}"
         binding.speedValue.text = "${fireball?.vel ?: "?"} km/s"
     }
 
-    fun calculateCraterRadius(impactEnergy: Double, altitude: Double, radiatedEnergy: Double, speed: Double) : Double{
-        return 0.0
+    private fun calculateCraterRadius(
+        impactEnergy: Double,
+        altitude: Double,
+        radiatedEnergy: Double,
+        speed: Double
+    ): Double {
+        val result = 0.05 * (impactEnergy / 4.184 * 10.0.pow(12).pow(0.33) )
+        return BigDecimal(result).setScale(2, RoundingMode.HALF_UP).toDouble()
+
+        //R=0,05× Energie en J / 4,184×10^12 * 0,33
+
     }
 
     fun formatDate(dateString: String): String {
@@ -88,7 +109,7 @@ class FireballViewFragment : Fragment() {
         return zonedDateTime.format(outputFormatter)
     }
 
-    fun mapButtonClicked(){
+    fun mapButtonClicked() {
         val parentFragmentTransaction = parentFragmentManager.beginTransaction()
         parentFragmentTransaction.replace(
             R.id.fireballFragmentContainer,
@@ -97,8 +118,8 @@ class FireballViewFragment : Fragment() {
         parentFragmentTransaction.commit()
     }
 
-    fun favButtonClicked(){
-        viewModel.isFavorite(fireball!!).observe( viewLifecycleOwner){
+    fun favButtonClicked() {
+        viewModel.isFavorite(fireball!!).observe(viewLifecycleOwner) {
             if (it) {
                 viewModel.removeFavorite(fireball!!)
                 fireball!!.isFavorite = false
