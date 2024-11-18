d72295b21aebdcc4a2d62c4621aaa93d721f44c9
yannis royer <royeryannis@gmail.com>
2024-11-18 21:18:42 +0100

    last one fireball

--- app/src/main/java/com/example/tp_mobile/views/fireball/FireballSectionFragment.kt
+++ app/src/main/java/com/example/tp_mobile/views/fireball/FireballSectionFragment.kt
@@ -17,16 +17,25 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.tp_mobile.MainActivity
 import com.example.tp_mobile.R
 import com.example.tp_mobile.utils.SortStyle
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 class FireballSectionFragment : Fragment() {
 
     private val viewModel: FireballListViewModel by viewModels()
 
+    private lateinit var dateRecyclerView: RecyclerView
     private lateinit var powerRecyclerView: RecyclerView
     private lateinit var powerAdapter: PowerAdapter
+    private lateinit var dateAdapter: DateAdapter
+
     private lateinit var barPowerText: TextView
     private lateinit var barPowerText2: TextView
     private lateinit var barPowerText3: TextView
+
+    private lateinit var dateText: TextView
+    private lateinit var dateText2: TextView
+    private lateinit var dateText3: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,16 +44,27 @@ class FireballSectionFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_fireball_section, container, false)
     }
 
-    @SuppressLint("SetTextI18n")
+    @SuppressLint("SetTextI18n", "CutPasteId")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         powerRecyclerView = view.findViewById(R.id.powerRecyclerView)
         powerRecyclerView.layoutManager = LinearLayoutManager(requireContext())
 
+        dateRecyclerView = view.findViewById(R.id.dateRecyclerView)
+        dateRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+
         barPowerText = view.findViewById(R.id.bar_power_text)
         barPowerText2 = view.findViewById(R.id.bar_power_text2)
         barPowerText3 = view.findViewById(R.id.bar_power_text3)
+
+        dateText = view.findViewById(R.id.last_one_date_text)
+        dateText2 = view.findViewById(R.id.last_one_date_text2)
+        dateText3 = view.findViewById(R.id.last_one_date_text3)
+
+
+
+
 
         powerAdapter = PowerAdapter(
             onClick = { fireball ->
@@ -59,6 +79,20 @@ class FireballSectionFragment : Fragment() {
         )
         powerRecyclerView.adapter = powerAdapter
 
+
+        dateAdapter = DateAdapter(onClick = { fireball ->
+            val parentFragmentTransaction = parentFragmentManager.beginTransaction()
+            parentFragmentTransaction.replace(
+                R.id.fireballFragmentContainer,
+                FireballViewFragment.newInstance(fireball)
+            )
+            parentFragmentTransaction.commit()
+        },
+            itemList = emptyList()
+        )
+        dateRecyclerView.adapter = dateAdapter
+
+
         viewModel.items.observe(viewLifecycleOwner) { fireballList ->
             if (fireballList.isNullOrEmpty()) {
                 Toast.makeText(requireContext(), "Aucune Fireball trouvée", Toast.LENGTH_SHORT).show()
@@ -67,19 +101,53 @@ class FireballSectionFragment : Fragment() {
                 powerAdapter.updateData(fireballList)
 
                 fireballList.getOrNull(0)?.let {
-                    barPowerText.text = "${it.impactE} Kt"
+                    barPowerText.text = "${it.energy}e10 J"
                 }
                 fireballList.getOrNull(1)?.let {
-                    barPowerText2.text = "${it.impactE} Kt"
+                    barPowerText2.text = "${it.energy}e10 J"
                 }
                 fireballList.getOrNull(2)?.let {
-                    barPowerText3.text = "${it.impactE} Kt"
+                    barPowerText3.text = "${it.energy}e10 J"
                 }
             }
         }
 
-        viewModel.fetchFireballData(3, 0, SortStyle.RADIATED_ENERGY_DESC)
+        viewModel.items.observe(viewLifecycleOwner) { fireballList ->
+            if (fireballList.isNullOrEmpty()) {
+                Toast.makeText(requireContext(), "Aucune Fireball trouvée", Toast.LENGTH_SHORT).show()
+                dateAdapter.updateData(emptyList())
+            } else {
+                dateAdapter.updateData(fireballList)
 
+                fireballList.getOrNull(0)?.let {
+                    dateText.text = "${it.date} date"
+                }
+                fireballList.getOrNull(1)?.let {
+                    dateText2.text = "${it.date} "
+                }
+                fireballList.getOrNull(2)?.let {
+                    dateText3.text = "${it.date} "
+                }
+            }
+        }
+
+
+        viewModel.fetchFireballData(3, 0, SortStyle.DATE_DESC)
+        viewModel.items.observe(viewLifecycleOwner) { fireballList ->
+            if (fireballList.isNullOrEmpty()) {
+                Toast.makeText(requireContext(), "Aucune Fireball trouvée", Toast.LENGTH_SHORT).show()
+            } else {
+                fireballList.getOrNull(0)?.let {
+                    dateText.text = it.date?.let { it1 -> formatDate(it1) }
+                }
+                fireballList.getOrNull(1)?.let {
+                    dateText2.text = it.date?.let { it1 -> formatDate(it1) }
+                }
+                fireballList.getOrNull(2)?.let {
+                    dateText3.text = it.date?.let { it1 -> formatDate(it1) }
+                }
+            }
+        }
         view.findViewById<Button>(R.id.listButton).setOnClickListener {
             val parentFragmentTransaction = parentFragmentManager.beginTransaction()
             parentFragmentTransaction.replace(
@@ -97,7 +165,7 @@ class FireballSectionFragment : Fragment() {
             requireActivity().overridePendingTransition(0, 0)
         }
     }
-
+    
     companion object {
         @JvmStatic
         fun newInstance() =
@@ -105,5 +173,15 @@ class FireballSectionFragment : Fragment() {
                 arguments = Bundle().apply {
                 }
             }
+    }
+    private fun formatDate(dateString: String): String {
+        return try {
+            val inputFormatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME
+            val outputFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+            val zonedDateTime = ZonedDateTime.parse(dateString, inputFormatter)
+            zonedDateTime.format(outputFormatter)
+        } catch (e: Exception) {
+            "Invalid Date"
+        }
     }
 }
