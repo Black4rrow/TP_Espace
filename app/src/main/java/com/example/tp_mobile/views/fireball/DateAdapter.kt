717df73068184bb6c941f65bb35c213f29d46e19
yannis royer <royeryannis@gmail.com>
2025-01-17 13:45:53 +0100

    fix : dateAdapter

--- app/src/main/java/com/example/tp_mobile/views/fireball/DateAdapter.kt
+++ app/src/main/java/com/example/tp_mobile/views/fireball/DateAdapter.kt
@@ -3,6 +3,7 @@ package com.example.tp_mobile.views.fireball
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tp_mobile.R
@@ -14,11 +15,8 @@ class DateAdapter(
 ) : RecyclerView.Adapter<DateAdapter.DateViewHolder>() {
 
     inner class DateViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val speedTextView: TextView = itemView.findViewById(R.id.speed_text)
-        val speedImageView: ImageView = itemView.findViewById(R.id.speed_hint)
-
-        val powerTextView: TextView = itemView.findViewById(R.id.power_text)
-        val powerImageView: ImageView = itemView.findViewById(R.id.power_hint)
+        val speedTextView: TextView = itemView.findViewById(R.id.top_date_speed_text)
+        val powerTextView: TextView = itemView.findViewById(R.id.top_date_power_text)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DateViewHolder {
